817e70bc303cee4833d0b22c6364d1ea298a97c4
@raspiduino <68118236+raspiduino@users.noreply.github.com>
2020-07-31 11:27:05 +0700

    Reduce 600 bytes of RAM
    Turn off receive serial data from computer

--- micropython_v2.0.ino
+++ micropython_v2.0.ino
@@ -8,7 +8,7 @@
 #include <AltSoftSerial.h>
 
 //String all_lcd_content = "";
-String command = "";
+//String command = "";
 String input = "";
 int x_char = 0;
 int y_char = 0;
@@ -22,7 +22,7 @@ AltSoftSerial esp8266;
 void printlcd();
 
 void setup() {
-  Serial.begin(9600); //Open serial port to computer
+  //Serial.begin(9600); //Open serial port to computer
   esp8266.begin(4800); //Open serial port to esp8266
   /*Reset the esp8266 module (reset pin of the esp8266 connected to digital pin 3 on Arduino Uno)*/
   pinMode(2, OUTPUT);
@@ -49,15 +49,15 @@ void setup() {
     pinMode(i, INPUT_PULLUP);   
   }
   esp8266.println(""); //Press enter to esp8266
-  Serial.setTimeout(5000); //Set the serial timeout to 5000ms to read all character
+  //Serial.setTimeout(5000); //Set the serial timeout to 5000ms to read all character
 }
 
 void loop() {
   /*Draw the input box for typing*/
-  lcd.DrawLine(0,39,83,39,BLACK);
+  //lcd.DrawLine(0,39,83,39,BLACK);
   lcd.Asc_Char(0,40,'>', BLACK);
   lcd.Rect(5,39,79,8,BLACK);//Clear the keys
-  lcd.Display();
+  //lcd.Display();
   /*Read the data from esp8266*/
   while (esp8266.available() > 0) { //If the serial has incomming character, read it
     int incomingByte = esp8266.read(); //Read the character ASCII number to the incomingByte varrible
@@ -65,7 +65,7 @@ void loop() {
       if (incomingByte == 13) { //If the user press enter
         //all_lcd_content = String(all_lcd_content + '\n');
         //Serial.println("Next line"); //For debug, in real run please comment this!
-        Serial.println("");
+        //Serial.println("");
         x_char = 0;
         y_char = y_char + 8;
       }
@@ -73,12 +73,12 @@ void loop() {
     } else {
       char esp8266_data = char(incomingByte); //Convert number to ASCII character
       //all_lcd_content = String(all_lcd_content + esp8266_data);
-      Serial.print(esp8266_data);
+      //Serial.print(esp8266_data);
       printlcd(esp8266_data);
     }
   }
   /*Receive the command from the main Arduino serial for use in Arduino IDE*/
-  while (Serial.available() > 0) { //If the serial has incomming character, read it
+  /*while (Serial.available() > 0) { //If the serial has incomming character, read it
     int incomingByte = Serial.read(); //Read the character ASCII number to the incomingByte varrible
     if (incomingByte == -1 | incomingByte == 10 | incomingByte == 13) {
       if (incomingByte == 13) { //If the user press enter
@@ -90,7 +90,7 @@ void loop() {
       char data = char(incomingByte); //Convert number to ASCII character
       command = String(command + data);
     }
-  }
+  }*/
   /************************Get keys from keyboard**************************/
   /***LEFT button***/
   if(digitalRead(3) == LOW){ //LEFT button is pushed
@@ -112,13 +112,13 @@ void loop() {
       esp8266.print(input); //Sent data to esp8266
       esp8266.println(""); //Press enter
       //lcd.Rect(5,39,79,8,BLACK);//Clear the keys
-      lcd.Display();
+      //lcd.Display();
       x_key = 6;
       enter = 0;
       input = "";
     } else{ //Not press enter
       input = String(input + char(key));
-      Serial.print(key);
+      //Serial.print(key);
       if(x_key < 80){ //Check if end of lcd
         x_key = x_key + 5;  
       } else{
@@ -147,21 +147,21 @@ void loop() {
 void printlcd(char data){
   if(x_char < 80 && y_char < 33){
     lcd.Asc_Char(x_char,y_char,data,BLACK);
-    lcd.Display();
+    //lcd.Display();
     x_char = x_char + 5;
   } else {
     if(x_char > 74){
       x_char = 0;
       y_char = y_char + 8;
       lcd.Asc_Char(x_char,y_char,data,BLACK);
-      lcd.Display();
+      //lcd.Display();
     }
     if(y_char > 32){
       lcd.Clear();
       x_char = 0;
       y_char = 0;
       lcd.Asc_Char(x_char,y_char,data,BLACK);
-      lcd.Display();
+      //lcd.Display();
     }
   }
 }
